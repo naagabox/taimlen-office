84c12cd8784a5a8ead0cdfc1cdf7239ff8267a8d
aspsptyd <devadiroot@gmail.com>
2026-05-12 07:16:12 +0700

    fix: issue a tree hydrated but some attributes of the server rendered HTML didn't match the client properties. This won't be patched up. This can happen if a SSR-ed Client Component used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} h-full antialiased`}>
+    <html lang="en" className={`${inter.variable} h-full antialiased`} suppressHydrationWarning>
       <body className="min-h-full flex flex-col font-sans">{children}</body>
     </html>
   );
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,13 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+    <ThemeProvider 
+      attribute="class" 
+      defaultTheme="light" 
+      enableSystem={false}
+      disableTransitionOnChange
+      storageKey="theme"
+    >
       <SessionProvider>{children}</SessionProvider>
     </ThemeProvider>
   )
